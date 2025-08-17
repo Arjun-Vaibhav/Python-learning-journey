212d8da79cf3ed6863bd650617aaf7922a35e340
Arjun-Vaibhav <arjunvaibhav81@gmail.com>
2025-08-17 19:11:07 +0530

    edit code

--- Functional Programming/abcd.py
+++ Functional Programming/abcd.py
@@ -1,68 +1,77 @@
+#========================================================================================
+
 #WAP which will generate n to 1 n is a positive integer value
 
-# i=int(input('Enter  Nth number:'))
-# if i<=0:
-#     print('invalid value n')
-# else:
-#     while (i>=1):
-#         print(i)
-#         i=i-1
+i=int(input('Enter  Nth number:'))
+if i<=0:
+    print('invalid value n')
+else:
+    while (i>=1):
+        print(i)
+        i=i-1
+#========================================================================================
 
 #WAP print the even nuumber to n number
-# n=int(input('Enter the number:'))
-# i=1
-# while i<n:
-#     if i%2==0:
-#         print(i)        
-#     i+=1         
+n=int(input('Enter the number:'))
+i=1
+while i<n:
+    if i%2==0:
+        print(i)        
+    i+=1         
     
-  
+#========================================================================================
+
 
 #WAP which will genarate decresing order
 
-# num=int(input('Enter number:'))
-# i=1
-# while num>=i:
-#     if num%2==0:
-#         print(num)
-#     num=num-1
+num=int(input('Enter number:'))
+i=1
+while num>=i:
+    if num%2==0:
+        print(num)
+    num=num-1
 
+#========================================================================================
 
-# num=int(input('Enter the number you want to write a table:'))
-
-# i=1
-# while i<=10:
-#     result=num*i
+num=int(input('Enter the number you want to write a table:'))
+i=1
+while i<=10:
+    result=num*i
     
-#     print('{} X {} = {}'.format(num,i,result))
-#     i=i+1
+    print('{} X {} = {}'.format(num,i,result))
+    i=i+1
+#========================================================================================
 
 
 #wap which will accept list of value and fine there sum of average
 
-# string=input('Enter input:')
-# i=0
-# while i<=len(string):
-#     if(string[i]=="o"):
-#         break
-#     else:
-#         print(string[i])
-#     i=i+1    
+string=input('Enter input:')
+i=0
+while i<=len(string):
+    if(string[i]=="o"):
+        break
+    else:
+        print(string[i])
+    i=i+1    
+#========================================================================================
 
-# n='MISSISIPI'
-# count=0
-# i=0
-# while i<=len(n):
-#     if n[i]=='i':
-#         count=count+1
-#         if count==2:
-#             break
-#             print(n[i])
-#             i=i+1
-# else:
-#     print('jay ho')
-  
+n='MISSISIPI'
+count=0
+i=0
+while i<=len(n):
+    if n[i]=='i':
+        count=count+1
+        if count==2:
+            break
+            print(n[i])
+            i=i+1
+else:
+    print('jay ho')
+#========================================================================================
+
 
 #WAP accept numerial value and check prime or not
+#========================================================================================
+
 #WAP which will accept a word uppercase latter lowercase latter and special sembol 
     
