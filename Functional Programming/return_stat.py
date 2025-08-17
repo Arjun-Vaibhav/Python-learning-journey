38c07c28e8e7c1bb9e386633ae3e5663eb004415
Arjun-Vaibhav <arjunvaibhav81@gmail.com>
2025-08-17 18:47:21 +0530

    modify file return_stat

--- Functional Programming/return_stat.py
+++ Functional Programming/return_stat.py
@@ -1,6 +1,4 @@
-'''
 
-'''
 #========================================================================================
 num=100
 def fun():
@@ -60,17 +58,18 @@ def isprime(num):
 print(isprime([1,2,3,4,5]))
 #========================================================================================
 
-# def is_prime(lst):
-#     prime_numbers=[]
+def is_prime(lst):
+    prime_numbers=[]
 
-#     for i in lst:
-#         for num in range(2,i):
-#             if i%num==0:
-#                 break
-#         else:
-#             prime_numbers.append(i)
-#     return prime_numbers
-# print(is_prime([1,2,3,4,5,6,7,8]))
+    for i in lst:
+        for num in range(2,i):
+            if i%num==0:
+                break
+        else:
+            prime_numbers.append(i)
+    return prime_numbers
+print(is_prime([1,2,3,4,5,6,7,8]))
+#========================================================================================
 
 
 '''
@@ -79,66 +78,77 @@ Nested function
 ===============================================================================================
 '''
 
-# def f1():
-#     print("welcome to function1.......")
-#     def f2():
-#         print("welcome to function f2")
-#     return f2
-# f2=f1()
-# f2()
+def f1():
+    print("welcome to function1.......")
+    def f2():
+        print("welcome to function f2")
+    return f2
+f2=f1()
+f2()
+#========================================================================================
 
 
-# def f1():
-#     print("welcome to function1.......")
-#     a=10
-#     def f2():
-#         print("welcome to function f2")
-#     return f2,a**2
-# f2,sq=f1()
-# print(sq)
-# f2()
+def f1():
+    print("welcome to function1.......")
+    a=10
+    def f2():
+        print("welcome to function f2")
+    return f2,a**2
+f2,sq=f1()
+print(sq)
+f2()
+#========================================================================================
 
-# def st_details(name,city):
-#     st_d=f'my name is {name} and I am from {city}'
-#     def inst_details(in_name,branch):
-#         int_d=f'''
-#         institute Name:{in_name}
-#         branch:{branch}
-#             '''
-#         return int_d
-#     return st_d,inst_details
-# st_d,inst_details=st_details('harish','nagpur')
-# int_d=inst_details('kiran academy','karve nagar')
-# print(st_d)
-# print(int_d)
+def st_details(name,city):
+    st_d=f'my name is {name} and I am from {city}'
+    def inst_details(in_name,branch):
+        int_d=f'''
+        institute Name:{in_name}
+        branch:{branch}
+            '''
+        return int_d
+    return st_d,inst_details
+st_d,inst_details=st_details('harish','nagpur')
+int_d=inst_details('kiran academy','karve nagar')
+print(st_d)
+print(int_d)
+#========================================================================================
 
-# def student(name,city,age):
-#     student_informaton=f'my name is {name} I am from {city} i am {age} year old'
-#     def teacher(name,department):
-#         teacher_inforamtion=f'my name is {name} I am wroking as a {department} department '
-#         return teacher_inforamtion
-#     teacher_info=teacher('mr vaibhav sir','CSIT')
-#     return student_informaton,teacher_info
+
+def student(name,city,age):
+    student_informaton=f'my name is {name} I am from {city} i am {age} year old'
+    def teacher(name,department):
+        teacher_inforamtion=f'my name is {name} I am wroking as a {department} department '
+        return teacher_inforamtion
+    teacher_info=teacher('mr vaibhav sir','CSIT')
+    return student_informaton,teacher_info
+#========================================================================================
+
     
-# stdudent_info,teacher_info=student('vaibhav','pune',21)
-# print(stdudent_info)
-# print(teacher_info)
+stdudent_info,teacher_info=student('vaibhav','pune',21)
+print(stdudent_info)
+print(teacher_info)
+#========================================================================================
+
 
 #Write a function to calculate the factorial of a number (use loop).
-# def fact(number):
-#     fact=1
-#     for i in range(1,number+1):
-#         fact=fact*i
-#         return fact
-# print(fact(5))
+def fact(number):
+    fact=1
+    for i in range(1,number+1):
+        fact=fact*i
+        return fact
+print(fact(5))
+#========================================================================================
 
 #Define a function that accepts a list and returns the sum of all elements.
-# def sum_list(lst):
-#     sum=0
-#     for i in lst:
-#         sum=sum+i
-#     return sum
-# print(sum_list([2,4]))
+def sum_list(lst):
+    sum=0
+    for i in lst:
+        sum=sum+i
+    return sum
+print(sum_list([2,4]))
+#========================================================================================
+
 
 #Create a function that takes a string and returns the number of vowels in it.
 
@@ -150,3 +160,4 @@ def vowel(string):
     return vowels
 string=input('enter any string:')
 print('this vowel found in exiting strig=',vowel(string))
+#========================================================================================
